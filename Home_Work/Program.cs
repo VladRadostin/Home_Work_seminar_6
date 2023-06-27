ce646203a7921038464558b77c4ea2a5e96a33c6
vlad <vladradostinn@yandex.ru>
2023-06-28 01:19:04 +0300

    Home_work_seminar_7

---
+++ Home_Work/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+
+int numbers = Math.Abs(Convert.ToInt32(Prompt("Введите сколько чисел хотели бы ввести: ")));
+double[] array = new double[numbers];
+int Count = 0;
+
+double Prompt (string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    double result = Convert.ToDouble(value);
+    return result;
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = Prompt($"Введите число номер {i+1}: ");
+    if (array[i] > 0)
+    {
+        Count++;
+    }
+}
+
+Console.WriteLine(" ");
+Console.WriteLine($"пользователь ввёл {Count} чисел больше 0");
---
+++ Home_Work_1/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double Prompt (string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    double result = Convert.ToDouble(value);
+    return result;
+}
+
+double b1 = Prompt("ведите точку прямой b1: ");
+double k1 = Prompt("ведите точку прямой k1: ");
+
+double b2 = Prompt("ведите точку прямой b2: ");
+double k2 = Prompt("ведите точку прямой k2: ");
+
+
+
+Console.Clear();
+
+
+
+double y = 0;
+double x = 0;
+
+Console.WriteLine($"точки прямой: k1 = {k1} , b1 = {b1} ; k2 = {k2} ; b2 = {b2}");
+
+
+x = (b2 - b1) / (k1 - k2);
+y = k1 * x + b1;
+
+Console.Write($"x = {x} ");
+Console.Write($"y = {y} ");
+
+Console.WriteLine(" ");
+Console.WriteLine($"точки пересечения двух прямых ({x} ; {y})");
